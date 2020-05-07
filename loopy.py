0804c2ade0dc4f85a9c137bb8ec82fb04fda3664
Parth Natekar <patnat26@gmail.com>
2020-05-07 11:48:51 +0530

    Updated for large images

--- loopy.py
+++ loopy.py
@@ -3,12 +3,13 @@ import pandas as pd
 from scipy.stats import gamma
 import matplotlib.pyplot as plt
 import cv2
+from sklearn.model_selection import ParameterGrid
 
 class Loopy():
 
 	def __init__(self, grid, iterations, params):
 		self.grid = grid
-		self.size = self.grid.shape[0]
+		self.height, self.width = self.grid.shape
 		self.iterations = iterations
 		self.params = params
 		self.compatibility_inter = np.array([[1.0, self.params['theta']], [self.params['theta'], 1.0]])
@@ -62,7 +63,7 @@ class Loopy():
 
 		return(neighbours)
 
-	def message_dict(self):
+	def messages_seq(self):
 
 		factor_messages = np.ones((self.size**2, self.size**2, 2))
 		clique_messages = np.ones((self.size**2, self.size**2, 2))
@@ -89,12 +90,12 @@ class Loopy():
 			for n in neighbours:
 				beta[np.unravel_index(j, (self.size, self.size))] *= factor_messages[j,n]
 
-		accuracy = np.sum(self.grid == np.argmax(beta, axis = 2))*1/self.size**2
+		accuracy = np.sum(self.grid == np.argmax(beta, axis = 2))/self.size**2
 		plt.imshow(np.argmax(beta, axis = 2))
 		plt.title('Denoised Image, Accuracy = {0:.2f}, Theta = {1:.2f}, Gamma = {2:.2f}'.format(accuracy, self.params['theta'], self.params['gamma']))
 		plt.show()
 
-	def message_dict_parallel(self):
+	def messages_parallel(self):
 
 		clique_messages_r = np.ones((self.size, self.size, 2))
 		clique_messages_l = np.ones((self.size, self.size, 2))
@@ -158,17 +159,17 @@ class Loopy():
 		# plt.title('Denoised Image, Accuracy = {0:.2f}, Theta = {1:.2f}, Gamma = {2:.2f}'.format(accuracy, self.params['theta'], self.params['gamma']))
 		plt.show()
 
-	def message_dict_sync(self):
+	def messages_sync(self):	
 
-		clique_messages = np.ones((self.size, self.size, 2, 4))
-		factor_messages = np.ones((self.size, self.size, 2, 4))
-		factor_norm = np.ones((self.size, self.size, 1, 4))
-		clique_norm = np.ones((self.size, self.size, 1, 4))
-		beta = np.ones((self.size, self.size, 2))
+		clique_messages = np.ones((self.height, self.width, 2, 4))
+		factor_messages = np.ones((self.height, self.width, 2, 4))
+		factor_norm = np.ones((self.height, self.width, 1, 4))
+		clique_norm = np.ones((self.height, self.width, 1, 4))
+		beta = np.ones((self.height, self.width, 2), dtype = 'float64')
 
-		for j in range(self.size**2):
-			beta[np.unravel_index(j, (self.size, self.size))] = self.compatibility_outer[:, self.grid[np.unravel_index(j, 
-				(self.size, self.size))]]
+		for j in range(self.height*self.width):
+			beta[np.unravel_index(j, (self.height, self.width))] = self.compatibility_outer[:, int(self.grid[np.unravel_index(j, 
+				(self.height, self.width))])].astype('float64')
 
 		for i in range(self.iterations):
 
@@ -185,15 +186,14 @@ class Loopy():
 				for ii in range(2):
 					factor_messages[:, :, ii, j] /= np.squeeze(factor_norm[:,:,:,j])
 
-			
-			clique_messages[:,:,:,0] *= self.compatibility_inter.dot(np.append(np.ones((self.size, 1, 2)), 
-										factor_messages[:,:-1,:, 0], axis=1).transpose((0,2,1))).transpose()
+			clique_messages[:,:,:,0] *= self.compatibility_inter.dot(np.append(np.ones((self.height, 1, 2)), 
+										factor_messages[:,:-1,:, 0], axis=1).transpose((0,2,1))).transpose(1,2,0)
 			clique_messages[:,:,:,1] *= self.compatibility_inter.dot(np.append(factor_messages[:, 1:, :, 1], 
-										np.ones((self.size, 1, 2)), axis = 1).transpose((0,2,1))).transpose()
+										np.ones((self.height, 1, 2)), axis = 1).transpose((0,2,1))).transpose(1,2,0)
 			clique_messages[:,:,:,2] *= self.compatibility_inter.dot(np.append(factor_messages[1:,:, :, 2], 
-										np.ones((1, self.size, 2)), axis = 0).transpose((0,2,1))).transpose()
-			clique_messages[:,:,:,3] *= self.compatibility_inter.dot(np.append(np.ones((1, self.size, 2)), 	
-										factor_messages[:-1,:, :, 3], axis = 0).transpose((0,2,1))).transpose()
+										np.ones((1, self.width, 2)), axis = 0).transpose((0,2,1))).transpose(1,2,0)
+			clique_messages[:,:,:,3] *= self.compatibility_inter.dot(np.append(np.ones((1, self.width, 2)), 	
+										factor_messages[:-1,:, :, 3], axis = 0).transpose((0,2,1))).transpose(1,2,0)
 
 			for j in range(clique_messages.shape[-1]):
 				clique_norm[:,:,:,j] = np.sum(clique_messages[:,:,:,j], axis=2)[..., None]
@@ -203,33 +203,52 @@ class Loopy():
 					clique_messages[:, :, ii, j] /= np.squeeze(clique_norm[:,:,:,j])
 
 		for j in range(clique_messages.shape[-1]):
-			beta *= np.append(np.ones((self.size, 1, 2)), factor_messages[:,:-1,:,0], axis=1)\
-					*np.append(np.ones((1, self.size, 2)), factor_messages[:-1,:,:,3], axis = 0)\
-					*np.append(factor_messages[1:,:,:,2], np.ones((1, self.size, 2)), axis = 0)\
-					*np.append(factor_messages[:, 1:,:,1], np.ones((self.size, 1, 2)), axis = 1)
+			beta *= np.append(np.ones((self.height, 1, 2)), factor_messages[:,:-1,:,0], axis=1)\
+					*np.append(np.ones((1, self.width, 2)), factor_messages[:-1,:,:,3], axis = 0)\
+					*np.append(factor_messages[1:,:,:,2], np.ones((1, self.width, 2)), axis = 0)\
+					*np.append(factor_messages[:, 1:,:,1], np.ones((self.height, 1, 2)), axis = 1)
 
-		plt.imshow(np.argmax(beta, axis = 2))
-		# plt.title('Denoised Image, Accuracy = {0:.2f}, Theta = {1:.2f}, Gamma = {2:.2f}'.format(accuracy, self.params['theta'], self.params['gamma']))
+		print(beta.dtype)
+		accuracy = np.sum((self.grid == np.argmax(beta, axis = 2))*1)/(self.height*self.width)
+		plt.imshow(np.argmax(beta, axis = 2), cmap = 'Greys_r')
+		plt.title('Denoised Image, Accuracy = {0:.2f}, Theta = {1:.2f}, Gamma = {2:.2f}'.format(accuracy, self.params['theta'], self.params['gamma']))
 		plt.show()
+		# return(accuracy)
 
 if __name__ == '__main__':
 
-	size = 200
-	flip_prob = 0.2
+	size = 100
+	flip_prob = 0.1
 	grid = np.zeros((size, size), dtype='int64')
 	for j in range(size**2):
 		idx = np.unravel_index(j, (size, size))
-		if ((idx[0]-50)**2+(idx[1]-50)**2)**0.5 <= 50:
+		if ((idx[0]-50)**2+(idx[1]-50)**2)**0.5 <= 25:
 			grid[idx] = 1
 		thresh = np.random.random_sample()
 		if thresh < flip_prob:
 			grid[idx] = 1-grid[idx]
 
-	plt.imshow(grid)
+	image = cv2.imread('binary.png', 0).astype('float64')/255
+	image = cv2.threshold(image, 0.5, 1, cv2.THRESH_BINARY)[1]
+	for j in range(image.shape[0]*image.shape[1]):
+		idx = np.unravel_index(j, (image.shape[0], image.shape[1]))
+		thresh = np.random.random_sample()
+		if thresh < flip_prob:
+			image[idx] = 1-image[idx]
+
+	plt.imshow(image, cmap = 'Greys_r')
 	plt.title('Noisy Image')
 	plt.show()
 
-	params = {'theta':0.4, 'gamma':0.2}
-	L = Loopy(grid, 100, params)
-	L.message_dict_sync()
+	params = {'theta':0.5, 'gamma':0.2}
+	L = Loopy(grid, 20, params)
+	L.messages_sync()
 
+	# parameters = {'theta':list(np.arange(0,1,0.1)), 'gamma':list(np.arange(0,1,0.1))}
+	# p_grid = ParameterGrid(parameters)	
+	# acc_dict = {}
+	# for params in p_grid:
+	# 	L = Loopy(grid, 50, params)
+	# 	acc_dict[tuple(params.values())] = L.message_dict_sync()
+
+	# print(acc_dict)
